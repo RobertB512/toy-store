0255c50753dedd137af1dbb4e7947edab0f63b7a
RobertB512 <rwbcool@gmail.com>
2023-07-18 13:17:55 -0700

    Add another disclaimer, add some shop button styling

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -7,12 +7,13 @@ export default function Header() {
   return (
     <header className="row">
       <section className="col-md-6 text-bg-primary introduction">
-        <h1>Simple Fun</h1>
-        <h2 className="slogan">You don't need screens to have fun</h2>
+        <h2 className="fs-2 text-start">Simple Fun</h2>
+        <h1 className="slogan">You don't need screens to have fun</h1>
         <p className="mb-2 opening">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque incidunt facilis voluptate dignissimos dolore ut similique quibusdam fugit, unde consequuntur, ea optio ratione quos eius, rerum nulla laboriosam nihil asperiores sequi provident. Dolorum laborum, quidem commodi eveniet facere itaque officiis corporis ea libero facilis veniam deleniti debitis, odio non eos.
         </p>
-        <p className="text-bg-danger fs-6 p-3 disclaimer"> Note: this is not a real toy store. There are no real toys that you can buy here. This site can not process any data or payment information. If you want to fill out the form in the cart, you could, and your information will not be saved. This site was just to practice making websites</p>
+        <p className="text-bg-danger fs-6 p-3 disclaimer"> Note: this is not a real toy store. There are no real toys that you can buy here. This site can not process any data or payment information. If you want to fill out the form in the cart, you could, and your information will not be saved. This site was just to practice making websites.</p>
+        <p className="text-bg-danger fs-6 p-3 disclaimer"> Note: the Latin throughout this site is fake text, so I could produce this site faster.</p>
       </section>
       <div className="col-md-6 image-area">
         <img className="header-img" src={trainIllustration} alt="An illustration of a toy train." />
--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -12,7 +12,7 @@ export default function Product(props) {
 
 	return (
 		<section className="container-fluid p-3">
-			<h2>Product: {toyTitle.toy}</h2>
+			<h2>{toyTitle.toy}</h2>
 			<div className="row justify-content-center product-info">
         <div className=" col-md-4 img-wrapper"><img className="product-page-img" src={`../${state.toyImg}`} alt={toyTitle.toy} /></div>
         <article className="col-md-8 w-100 toy-details">
@@ -20,7 +20,7 @@ export default function Product(props) {
           <div className="row purchase-area">
             <p className="col-2 text-bg-dark price">${state.toyPrice}</p>
             <button
-              className="col-2 w-auto border-0 button text-bg-primary add-to-cart-btn"
+              className="col-2 w-auto border-0 btn btn-primary add-to-cart-btn"
               onClick={() => setCartItems([...cartItems, {toyTitle: toyTitle.toy, toyImg: state.toyImg, toyPrice: state.toyPrice}])
               }>
               Add to cart
